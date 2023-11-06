863e2ff748fdb07d752acd2a7434dfc5f12aaa0f
AOrps <aorbeandrews@gmail.com>
2023-11-05 22:20:50 -0500

    plz work
    Signed-off-by: AOrps <aorbeandrews@gmail.com>

--- gambit-toml.scm
+++ gambit-toml.scm
@@ -7,14 +7,16 @@
 ;; TODO: build toml parser
 
 
-(define-library (https://github.com/AOrps gambit-toml)
+(define-library (gambit-toml)
 
-  (export func1 func2)
+  (import (scheme base)
+          (scheme write))
+  
+  (export func1
+          func2)
 
   (begin
 
     (define (func1 val) (* val 2))
-
     (define (func2 x y z) (+ x y z))
-    
 ))
